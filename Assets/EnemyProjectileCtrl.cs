c5b96f72bbe29e513b98fb58204c59566fdbbda9
spriggan4 <38972792+spriggan4@users.noreply.github.com>
2018-06-25 17:04:36 +0900

    no message

---
+++ Assets/EnemyProjectileCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileCtrl : MonoBehaviour {
+
+    [SerializeField]
+    private float projectileSpeed;
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        direction = this.transform.forward;
+    }
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(direction * (projectileSpeed * 10));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Weapon"&&other.tag!="Enemy")
+        {
+            Debug.Log("발사체 충돌 = " + other.name);
+            Destroy(this.gameObject);
+        }
+    }
+}
