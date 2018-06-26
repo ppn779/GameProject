7afcde29e66a137db4d55b9202a11bca5423e0ac
황재석 <spriggan4@hotmail.com>
2018-06-26 19:14:54 +0900

    no message

---
+++ Assets/EntranceColliderCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceColliderCtrl : MonoBehaviour {
+    private BoxCollider boxCollider;
+
+	// Use this for initialization
+	void Start () {
+        boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider == null) { Debug.LogError("입구 트리거 콜라이더 null"); }
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            boxCollider.isTrigger = false;
+        }
+    }
+}
