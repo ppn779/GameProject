42dc2485333ee4d208f8be3b6bb80b06b170e421
K-Hoom <index321@naver.com>
2018-06-29 11:42:29 +0900

    commit

---
+++ Assets/SkeltonBossAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeltonBossAttack : MonoBehaviour
+{
+
+    public GameObject TheEnemy;
+    public int AttackTrigger;
+    public int DealingDamage;
+
+    void Update()
+    {
+        if (AttackTrigger == 0)
+        {
+            TheEnemy.GetComponent<Animation>().Play("Walk");
+        }
+        if (AttackTrigger == 1)
+        {
+            if (DealingDamage == 0)
+            {
+                TheEnemy.GetComponent<Animation>().Play("Attack");
+                StartCoroutine(TakingDamage());
+            }
+        }
+    }
+
+
+    IEnumerator TakingDamage()
+    {
+        DealingDamage = 2;
+        yield return new WaitForSeconds(0.5f);
+
+    }
+}
