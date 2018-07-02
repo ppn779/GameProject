74e8be0679ae1bf175f4903b68f0427a36a335bc
K-Hoom <index321@naver.com>
2018-07-02 19:07:41 +0900

    07시 커밋

--- Assets/EndingDoorCtrl.cs
+++ Assets/EndingDoorCtrl.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EndingDoorCtrl : MonoBehaviour {
     public GameObject obj= null;
+    public Animation[] ani = null;
 
 	private void Update()
     {
         if (!obj)
         {
+            ani[0].Play("Door_Left");
+            ani[1].Play("Door_Right");
             Destroy(this.gameObject);
         }
     }
